21ca3fa16430463e98aae5d3357bd3d88a15ec19
GowthamNagaraj <84234435+GowthamNagaraj@users.noreply.github.com>
2025-06-04 17:44:14 +0530

    basic event update
    basic event update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,34 @@
+"use client"
+
+import { useState } from "react";
+
 export default function Home() {
+  const [form, setForm] = useState({
+    username:"",
+    password:""
+  })
+
+  const [error, setError] = useState("")
+
+  function handleLogin(e){
+    e.preventDefault();
+    if (form.username == "") {
+      setError("Please enter your username")
+    }
+    else if (form.password == "") {
+      setError("Please enter your password")
+    }
+    else {
+      alert("Login Success");
+      setError("");
+      console.log(form);
+      setForm({
+        username:"",
+        password:""
+      })
+    }
+  }
+
   return (
     <div className="container bg-slate-50 w-full h-full">
       <div className="flex items-center justify-center border w-full h-lvh">
@@ -6,12 +36,14 @@ export default function Home() {
           <div className="font-bold text-4xl">
             <h1 className="text-black" >LOGIN</h1>
           </div>
-          <div className="text-center flex flex-col gap-4">
-            <input type="text" name="username" id="username" placeholder="Username" className="border w-72 h-10 pl-2 text-xl border-slate-300 text-slate-800" />
-            <input type="text" name="password" id="password" placeholder="Password" className="border w-72 h-10 pl-2 text-xl border-slate-300 text-slate-800"/>
+          <div className="flex flex-col gap-4">
+            <input type="text" name="username" id="username" placeholder="Username" className="border w-72 h-10 pl-2 text-xl border-slate-300 text-slate-800" value={form.username} onChange={(e)=>setForm((prevState)=>({...prevState,username:e.target.value}))}/>
+            <span className="text-red-700 text-xs -mt-1 ml-2">{error}</span>
+            <input type="text" name="password" id="password" placeholder="Password" className="border w-72 h-10 pl-2 text-xl border-slate-300 text-slate-800" value={form.password} onChange={(e)=>setForm((prevState)=>({...prevState,password:e.target.value}))}/>
+            <span className="text-red-700 text-xs -mt-1 ml-2">{error}</span>
           </div>
             <div className="flex justify-end w-72">
-              <button className="border w-28 h-12 text-xl cursor-pointer hover:text-slate-50 hover:bg-black active:bg-gray-500 rounded-md">Login</button>
+              <button className="border w-28 h-12 text-xl text-slate-950 cursor-pointer hover:text-slate-50 hover:bg-black active:bg-gray-500 rounded-md" onClick={(e)=>handleLogin(e)}>Login</button>
             </div>
         </div>
       </div>
